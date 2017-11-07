bf8457fdd65f7206809b480ec295094385add682
Mike Kruk <tamagokun@gmail.com>
2017-11-06 19:04:56 -0500

    first pass at API response examples

--- data/api/response.js
+++ data/api/response.js
@@ -1,244 +1,267 @@
-const init = `
-require 'fridge_api'
+const site = {
+  id: 1,
+  name: 'Website',
+  date_created: '2015-01-01 01:00:00',
+  date_updated: '2015-01-01 01:00:00',
+  endpoints: [],
+  clients: [],
+  webhooks: []
+}
 
-fridge = FridgeApi.client({
-  :client_id => "sk_xxxxxxxxxxx"
-  :client_secret => "xxxxxxxxxxx"
-})
-`
+const ok = {
+  status: 200
+}
+
+const type = {
+  id: 3,
+  site_id: 1,
+  name: 'Post',
+  date_created: '2015-01-01 01:00:00',
+  date_updated: '2015-01-01 01:00:00',
+  display_order: 0,
+  options: {},
+  parts: [
+    {
+      name: 'title',
+      label: 'Title',
+      type: 'text'
+    }
+  ]
+}
+
+const cont = {
+  id: 15,
+  site_id: 1,
+  document_definition_id: 3,
+  date_created: '2015-02-01 01:00:00',
+  date_updated: '2015-02-01 01:00:00',
+  display_order: 0,
+  active: 1,
+  content: {
+    10: 'My Post Title',
+    11: '2015-02-01'
+  },
+  slug: 'my_post_title',
+  user_id: 90
+}
+
+const role = {
+  id: 7,
+  site_id: 1,
+  name: 'Member',
+  slug: 'member',
+  date_created: '2015-01-01 01:00:00',
+  date_updated: '2015-01-01 01:00:00',
+  display_order: 0,
+  options: {},
+  parts: [
+    {
+      name: 'photo',
+      label: 'Photo',
+      type: 'image'
+    }
+  ],
+  admin_access: false,
+  permissions: {}
+}
+
+const user = {
+  id: 21,
+  date_created: '2015-02-01 01:00:00',
+  date_updated: '2015-02-01 01:00:00',
+  active: true,
+  content: [],
+  email: 'support@fridgecms.com'
+}
 
 const sites = [
   {
     name: 'site.list',
-    code: `${init}
-sites = fridge.get("sites")`
+    res: [
+      site,
+      {
+        id: 2,
+        name: 'Other Website',
+        date_created: '2015-01-01 01:00:00',
+        date_update: '2015-01-01 01:00:00',
+        endpoints: [],
+        clients: [],
+        webhooks: []
+      }
+    ]
   },
   {
     name: 'site.get',
-    code: `${init}
-site = fridge.get("sites/1")
-`
+    res: site
   },
   {
     name: 'site.create',
-    code: `${init}
-site = fridge.post("sites", {
-  :name => "My Website"
-})
-`
+    res: site
   },
   {
     name: 'site.update',
-    code: `${init}
-site = fridge.get("sites/1")
-site.webhooks = ["http://example.com"]
-fridge.put("sites/1", site.commit())
-`
+    res: {...site, webhooks: ['http://example.com']}
   },
   {
     name: 'site.delete',
-    code: `${init}
-fridge.delete("sites/1")`
+    res: ok
   },
   {
     name: 'site.keys',
-    code: `${init}
-site = fridge.put("sites/1/regenerate_keys")
-`
+    res: {...site, clients: [
+      {
+        key: 'pk_xxxxxxxxxxx',
+        secret: '',
+        type: 'public'
+      },
+      {
+        key: 'sk_xxxxxxxxxxx',
+        secret: 'xxxxxxxxxxx',
+        type: 'private'
+      }
+    ]}
   }
 ]
 
 const types = [
   {
     name: 'type.create',
-    code: `${init}
-content_type = fridge.post("types", {
-  :name => "Post",
-  :parts => [
-    {
-      :name => "title",
-      :label => "Title",
-      :type => "text"
-    }
-  ]
-})`
+    res: type
   },
   {
     name: 'type.update',
-    code: `${init}
-content_type = fridge.put("types/3", {
-  :name => "Post",
-  :parts => [
-    {
-      :name => "title",
-      :label => "Title",
-      :type => "text"
-    },
-    {
-      :name => "published",
-      :label => "Published",
-      :type => "date"
+    res: {...type,
+      parts: [...type.parts, {
+        name: 'published',
+        label: 'Publish Date',
+        type: 'date'
+      }]
     }
-  ]
-})`
   },
   {
     name: 'type.delete',
-    code: `${init}
-fridge.delete("types/3")
-`
+    res: ok
   },
   {
     name: 'type.list',
-    code: `${init}
-types = fridge.get("types")`
+    res: [type, {
+      id: 4,
+      site_id: 1,
+      name: 'Event',
+      date_created: '2015-01-01 01:00:00',
+      date_updated: '2015-01-01 01:00:00',
+      display_order: 1,
+      options: {},
+      parts: [
+        {
+          id: 12,
+          name: 'location',
+          label: 'Location',
+          hint: 'Where the event will be held',
+          type: 'text',
+          required: true
+        }
+      ]
+    }]
   }
 ]
 
 const content = [
   {
     name: 'content.create',
-    code: `${init}
-post = fridge.post("content/type/post", {
-  :content => {
-    :title => "My Post Title",
-    :published => "2015-02-01"
-  },
-  :active => true
-})`
+    res: cont
   },
   {
     name: 'content.get',
-    code: `${init}
-post = fridge.get("content/15")`
+    res: cont
   },
   {
     name: 'content.update',
-    code: `${init}
-post = fridge.put("content/15", {
-  :content => {
-    :title => "Draft Title",
-    :published => "2015-02-01"
-  },
-  :active => false
-})`
+    res: {...cont, active: false, content: {
+      10: 'Draft Title',
+      11: '2015-02-01'
+    }}
   },
   {
     name: 'content.delete',
-    code: `${init}
-fridge.delete("content/15")`
+    res: ok
   },
   {
     name: 'content.list',
-    code: `${init}
-posts = fridge.get("content", {type: "post"})
-`
+    res: [cont]
   },
   {
     name: 'content.upload',
-    code: `${init}
-fridge.post("content/upload", {:file => file})`
+    res: {message: 'filename.jpg', status: 200}
   },
   {
     name: 'content.file',
-    code: `${init}
-file = fridge.get("content/upload/my_filename.txt")`
+    res: 'Binary'
   },
   {
     name: 'content.public',
-    code: `${init}
-post = fridge.post("public/post", {
-  :content => {
-    :title => "User submitted post",
-    :published => "2015-02-02"
-  },
-  :active => true
-})`
+    res: {
+      id: 23,
+      site_id: 1,
+      document_definition_id: 3,
+      date_created: '2015-02-01 01:00:00',
+      date_updated: '2015-02-01 01:00:00',
+      display_order: 0,
+      active: 1,
+      content: {
+        10: 'User submitted post',
+        11: '2015-02-01'
+      },
+      slug: 'user_submitted_post'
+    }
   }
 ]
 
 const roles = [
   {
     name: 'role.get',
-    code: `${init}
-member_role = fridge.get("roles/member")`
+    res: role
   },
   {
     name: 'role.create',
-    code: `${init}
-member_role = fridge.post("roles", {
-  :name => "Member",
-  :parts => [
-    {
-      :name => "avatar",
-      :label => "Avatar",
-      :type => "image"
-    }
-  ]
-})`
+    res: role
   },
   {
     name: 'role.update',
-    code: `${init}
-fridge.put("roles/member", {
-  :permissions => {
-    :content_type => [:read],
-    :content => [:read, :create, :update, :delete]
-  }
-})
-`
+    res: {...role}
   },
   {
     name: 'role.delete',
-    code: `${init}
-fridge.delete("roles/member")`
+    res: ok
   },
   {
     name: 'role.list',
-    code: `${init}
-user_roles = fridge.get("roles")
-`
+    res: [role]
   }
 ]
 
 const users = [
   {
     name: 'user.create',
-    code: `${init}
-user = fridge.post("users/role/member", {
-  :email => "support@fridgecms.com",
-  :active => true
-})
-`
+    res: user
   },
   {
     name: 'user.update',
-    code: `${init}
-user = fridge.put("users/21", {
-  :active => false
-})
-`
+    res: {...user}
   },
   {
     name: 'user.delete',
-    code: `${init}
-fridge.delete("users/21")
-`
+    res: ok
   },
   {
     name: 'user.get',
-    code: `${init}
-user = fridge.get("users/21")
-`
+    res: user
   },
   {
     name: 'user.list',
-    code: `${init}
-members = fridge.get("users/role/member")
-`
+    res: [user]
   }
 ]
 
 const list = [...sites, ...types, ...content, ...roles, ...users]
-
 export default (name) => list.find(item => item.name === name)
